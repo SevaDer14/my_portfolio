6b7f70a5582054f814a877f5c8f859c3844ce792
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-07 19:31:04 +0200

    Adds Header and Footer with some semantic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Hello from './Hello'
+import Header from './Header'
+import Footer from './Footer'
 
 const App = () => {
   return (
-    <h1>
-      Hello Boi!
-    </h1>
+    <>
+      <Header/>
+      <Hello/>
+      <Footer/>
+    </>
   );
 }
 
