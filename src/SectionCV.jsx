6f4bfd69a50df347397bfbaecdd264baeeefed2f
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-11 12:26:36 +0200

    displays data but implementation sucks

--- src/SectionCV.jsx
+++ src/SectionCV.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Container, Divider, Grid, Header, Segment } from "semantic-ui-react";
 
-const GetData = ({section}) => {
+const GetData = ({section}) => {   
+  let output = JSON.stringify(section.items[0].data)
+  output = output.replace(/{/g, '')
+  output = output.replace(/}/g, '')
+  output = output.replace(/"/g, '')
+  output = output.replace(/:/g, ': ')
+  let result = output.split(',')
   return (
-    'p'
-  )
+    <>
+    {result.map(txt => <p>{txt}</p>)}
+    </>
+  )   
 }
 
 const SectionCV = ({section}) => {
@@ -16,31 +24,16 @@ const SectionCV = ({section}) => {
       </Header>
       <Grid columns={2} relaxed="very">
         <Grid.Column>
-          <Header as='h3' data-cy={`${section.data.title}`}>            
-            {`${section.data[0].title}:`}
+          <Header as='h3' data-cy={`${section.items[0].title}`}>            
+            {`${section.items[0].title}:`}
           </Header>
         </Grid.Column>
         <Grid.Column>                      
             <GetData section={section}/>         
         </Grid.Column>
-      </Grid>
-      <p></p>
+      </Grid>      
     </Container>
   )
 }
 
-/*
-const ProjectCard = ({ project }) => {
-  return (
-    <Card raised>
-      <Image src={project.image} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{project.name}</Card.Header>
-        <Card.Description>{project.description}</Card.Description>
-        <GenerateButtons project={project} />
-      </Card.Content>
-    </Card>
-  );
-};
-*/
 export default SectionCV
